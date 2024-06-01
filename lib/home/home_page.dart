7ec46c7bd0c03792f5c130bd966db1d68c995422
saraimess <158505957+saraimess@users.noreply.github.com>
2024-06-02 00:09:29 +0200

    Update home_page.dart

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,14 +1,50 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:xinner/home/widgets.dart/articles_horizontal_list.dart';
+import 'package:xinner/home/widgets.dart/services_horizontal_list.dart';
 import 'package:xinner/home/widgets.dart/upcoming_appointment.dart';
 import 'package:xinner/utils/constants/my_constants.dart';
 
-import 'widgets.dart/services_horizontal_list.dart';
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _username = '';
+  String _profilePictureUrl = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserData();
+  }
+
+  Future<void> _fetchUserData() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      DocumentSnapshot<Map<String, dynamic>> userData = await FirebaseFirestore
+          .instance
+          .collection('users')
+          .doc(user.uid)
+          .get();
+
+      String? fullName = userData['name'];
+      if (fullName != null && fullName.isNotEmpty) {
+        List<String> nameParts = fullName.split(' ');
+        _username = nameParts.first;
+      } else {
+        _username = '';
+      }
+
+      _profilePictureUrl = userData['profilePicture'] ?? '';
+
+      setState(() {});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +65,7 @@ class HomePage extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          "Hello, Yousra 👋️",
+                          "Hello, $_username 👋️",
                           style: const TextStyle(
                             color: Colors.black,
                             fontSize: 25,
@@ -49,20 +85,20 @@ class HomePage extends StatelessWidget {
                     Spacer(),
                     Align(
                       alignment: Alignment.topRight,
-                      child: ClipOval(
-                        child: Image.asset(
-                          "assests/images/photo.jpeg",
-                          fit: BoxFit.cover,
-                          width: 50,
-                          height: 50,
-                        ),
-                      ),
+                      child: _profilePictureUrl.isNotEmpty
+                          ? CircleAvatar(
+                              radius: 25,
+                              backgroundImage: NetworkImage(_profilePictureUrl),
+                            )
+                          : CircleAvatar(
+                              radius: 25,
+                              backgroundImage:
+                                  AssetImage("assests/images/photo.jpeg"),
+                            ),
                     ),
                   ],
                 ),
-                SizedBox(
-                  height: 50,
-                ),
+                SizedBox(height: 50),
                 Text(
                   "Your upcoming appointment",
                   style: TextStyle(
@@ -72,13 +108,9 @@ class HomePage extends StatelessWidget {
                     fontFamily: 'Roboto',
                   ),
                 ),
-                SizedBox(
-                  height: 12,
-                ),
+                SizedBox(height: 12),
                 UpcomingAppointment(),
-                SizedBox(
-                  height: 20,
-                ),
+                SizedBox(height: 20),
                 Text(
                   "Our services",
                   style: TextStyle(
@@ -88,9 +120,9 @@ class HomePage extends StatelessWidget {
                     fontFamily: 'Roboto',
                   ),
                 ),
-                const SizedBox(height: 17),
+                SizedBox(height: 17),
                 ServicesHorizontalList(),
-                const SizedBox(height: 20),
+                SizedBox(height: 20),
                 Text(
                   "Articles",
                   style: TextStyle(
@@ -100,9 +132,9 @@ class HomePage extends StatelessWidget {
                     fontFamily: 'Roboto',
                   ),
                 ),
-                const SizedBox(height: 17),
+                SizedBox(height: 17),
                 ArticlesHorizontalList(),
-                const SizedBox(height: 60),
+                SizedBox(height: 60),
               ],
             ),
           ),
@@ -111,3 +143,4 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
